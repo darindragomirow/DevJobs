8d2f28a9baa1848ca0d14383c675b5598f7f4825
Darin Dragomirow <darindragomirow@gmail.com>
2017-10-22 23:19:23 +0300

    Added Custom Error Page

---
+++ DevJobs.Web.Tests/Controllers/ErrorControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using DevJobs.Web.Controllers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DevJobs.Web.Tests.Controllers
+{
+    [TestFixture]
+    public class ErrorControllerTests
+    {
+        [Test]
+        public void Index_ShouldReturnViewResult()
+        {
+            //Arrange
+            var controller = new ErrorController();
+
+            //Act
+            var result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void NotFound_ShouldReturnViewResult()
+        {
+            //Arrange
+            var controller = new ErrorController();
+
+            //Act
+            var result = controller.NotFound() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
